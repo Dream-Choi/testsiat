6f5cd8d40769826797e9d30d38c3d57f4b191276
Dream-Choi <cmb02236@gmail.com>
2025-04-17 15:11:35 +0900

    feat: TodoRequestDTO에 Swagger 주석 추가

--- src/main/java/com/siat/testsiat/test/model/dto/TodoRequestDTO.java
+++ src/main/java/com/siat/testsiat/test/model/dto/TodoRequestDTO.java
@@ -1,5 +1,6 @@
 package com.siat.testsiat.test.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +14,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @Setter
 @Getter
+@Schema(description = "Todo 요청 DTO")
 public class TodoRequestDTO {
+    @Schema(description = "오라클 시퀀스 번호", example = "1")
     private int seq;
+    
     private String title;
     private String content;
     private String startDate;
